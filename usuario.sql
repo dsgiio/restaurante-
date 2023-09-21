e7810ca28eca9ddd1064092ec3698312e8947618
BarbMag <141353677+BarbMag@users.noreply.github.com>
2023-09-21 15:16:21 -0300

    Add files via upload
    atualizações 
    by: barbara 21_09

---
+++ usuario.sql
@@ -0,0 +1,9 @@
+create database restaurantedelas;
+use restaurantedelas;
+create table usuario (
+nome varchar(255),
+email varchar(255) primary key,
+senha varchar(255));
+select * from usuario;
+
+drop database restaurantedelas;
